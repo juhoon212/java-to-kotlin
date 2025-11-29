01be558985814c1c2baf110c7e84d3079b83e967
juhoon212 <ws5501@naver.com>
2025-11-29 20:06:43 +0900

    study: kotlin에서 상속을 하려면 open keyword를 붙여줘야 한다.

---
+++ src/main/kotlin/exception/_Exception.kt
@@ -0,0 +1,30 @@
+package com.example.exception
+
+fun main() {
+    //Thread.sleep(1) // checked exception 처리 강제하지 않음.
+
+    /*try {
+        Thread.sleep(1)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        println("Finally")
+    }*/
+
+    val a = try { // kotlin 에서는 try-catch 가 expression 임 (값을 반환함)
+        "1234".toInt()
+    } catch (e: Exception) {
+        println("예외 발생!")
+    }
+    println(a)
+
+    // throw Exception("예외 발생!")
+    val b: String? = null
+    val c: String = b ?: failFast("a is null")
+
+    println(c.length)
+}
+
+fun failFast(message: String): Nothing {
+    throw IllegalArgumentException(message)
+}
