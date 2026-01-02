c6e9d696d56f7c709185d367a85c334c4240418d
juhoon212 <ws5501@naver.com>
2026-01-02 23:05:55 +0900

    study: add higher-order function forEachStr and lambda expressions

--- src/main/kotlin/functional_programming/_FunctionalProgramming.kt
+++ src/main/kotlin/functional_programming/_FunctionalProgramming.kt
@@ -8,13 +8,30 @@ fun main() {
     result()
 
     call(printHello) //
+
+    val list = listOf("a", "b", "c")
+    val printAction: (String) -> Unit = { println(it) }
+    forEachStr(list, printAction)
 }
 
 val printHello: () -> Unit = { println("Hello") }
+
+/**
+ * 람다 표현식
+ */
+val printMessage: (String) -> Unit = { message: String -> println(message) }
+val printMessage2: (String) -> Unit = { message -> println(message) }
+val printMessage3: (String) -> Unit = { println(it) }
 
 /**
  * 고차 함수: 함수를 매개변수로 받는 함수
  */
 fun call(block: () -> Unit) {
     block()
+}
+
+fun forEachStr(collection: Collection<String>, action: (String) -> Unit) {
+    for (item in collection) {
+        action(item)
+    }
 }
