b61716eb8561f0b8db510260819b4bb94e3be0a4
juhoon212 <ws5501@naver.com>
2026-01-03 11:32:53 +0900

    study: add examples of outer functions and lambda expressions

--- src/main/kotlin/functional_programming/_FunctionalProgramming.kt
+++ src/main/kotlin/functional_programming/_FunctionalProgramming.kt
@@ -12,7 +12,37 @@ fun main() {
     val list = listOf("a", "b", "c")
     val printAction: (String) -> Unit = { println(it) }
     forEachStr(list, printAction)
+
+    outerFunction() // 실행은 되지만 반환된 함수는 사용되지 않음
+    val func = outerFunction()
+    func()
+
+    val func2 = outerFunctionLambda()
+    func2()
+
+    val func3 = outerFunctionLambda2()
+    func3()
+
+    // 함수중에 마지막에 람다식이 오면 소괄호 밖으로 뺄 수 있다.
+    forEachStr(list) {
+        println(it)
+    }
+
+    val callReference = ::printHello
+    callReference()()
+
+    val numberList = listOf("1", "2", "3")
+    numberList.map { it.toInt() }.forEach { println(it) }
+
+    // 참조로 표현
+    numberList.map(String::toInt).forEach(::println)
 }
+
+// 람다 표현식 전체
+val sum: (Int, Int) -> Int = { x:Int, y:Int -> x + y }
+
+// 위 람다식을 최대로 줄인 형태
+val sum2 = { x: Int, y:Int -> x + y }
 
 val printHello: () -> Unit = { println("Hello") }
 
@@ -29,6 +59,18 @@ val printMessage3: (String) -> Unit = { println(it) }
 fun call(block: () -> Unit) {
     block()
 }
+
+fun outerFunction(): () -> Unit {
+    return fun () {
+        println("This is an outer function returning a function")
+    }
+}
+
+fun outerFunctionLambda(): () -> Unit {
+    return { println("This is an outer function lambda") }
+}
+
+fun outerFunctionLambda2() : () -> Unit = { println("This is an outer function lambda2") }
 
 fun forEachStr(collection: Collection<String>, action: (String) -> Unit) {
     for (item in collection) {
